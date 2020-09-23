f0fbc9a78edeb478f8747933a67be067b398b974
Aymes <aysikh@MacBook-Pro.fios-router.home>
2020-09-23 12:01:38 -0400

    Finished up with blogger's validations

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -1,23 +1,49 @@
 class BloggersController < ApplicationController
+  before_action :find_blogger, only: [:show, :edit, :delete, :update]
+
   def index
-    
+    @bloggers = Blogger.all
   end
 
   def show
+  #   id = params[:id]
+  #   @blogger = Blogger.find(id)
+  end
+
+  def new
+    @blogger = Blogger.new
   end
 
   def create
+    # name = params["blogger"]["name"]
+    # age = params["blogger"]["age"]
+    # bio = params["blogger"]["bio"]
+    @blogger = Blogger.new(blogger_params)
+    if @blogger.save
+      redirect_to blogger_path(@blogger.id)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def new
+  def update
   end
 
   def delete
   end
 
-  def update
+
+  private
+
+  def blogger_params
+    params.require(:blogger).permit(:name, :age, :bio)
   end
+
+  def find_blogger
+    @blogger = Blogger.find(params[:id])
+  end
+
 end
--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -3,5 +3,7 @@ class Blogger < ApplicationRecord
   has_many :posts
   has_many :destinations, through: :posts
 
-
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :age, presence: true, numericality: {greater_than: 0}
+  validates :bio, length: {minimum: 30}
 end
