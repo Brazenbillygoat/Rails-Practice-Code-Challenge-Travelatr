a21aa7ebabfc5710d86211ddfc85a3d24dc47217
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-23 10:50:16 -0400

    Added controllers for each model and their routes

---
+++ app/controllers/bloggers_controller.rb
@@ -0,0 +1,23 @@
+class BloggersController < ApplicationController
+  def index
+    
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def delete
+  end
+
+  def update
+  end
+end
